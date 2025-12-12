df3c93a09735e7525e674444d7554570832ee0c4
Raúl Bueno <rbuegar928@g.educaand.es>
2025-12-12 14:46:59 +0100

    rbuegar928. 12/12/2025 Comentarios de JavaDoc del ejercicio de programación como elemento de mejora.

--- BuenoRaul_ActividadesUD3.odt/Actividad_02_UD3_ED/src/actividad_02_ud3_ed/ElementoMejoraEjercicioProg.java
+++ BuenoRaul_ActividadesUD3.odt/Actividad_02_UD3_ED/src/actividad_02_ud3_ed/ElementoMejoraEjercicioProg.java
@@ -2,32 +2,53 @@ package actividad_02_ud3_ed;
 
 import java.util.Scanner;
 
+/**
+ * Clase para comparar números y decir cuál de los dos o los tres es el mayor.
+ */
 public class ElementoMejoraEjercicioProg {
-    public static int mayorMenor(int num1, int num2){
+
+    /**
+     * Devuelve el número mayor entre dos números
+     * @param num1 el primer número
+     * @param num2 el segundo número
+     * @return el número más grande
+     */
+    public static int mayorMenor(int num1, int num2) {
         int maximo;
-        if (num1<num2){
-        maximo = num2;
+        if (num1 < num2) {
+            maximo = num2;
+        } else {
+            maximo = num1;
         }
-        else{
-        maximo = num1;
-        }
-        return(maximo);
+        return maximo;
     }
-    public static int mayorMenor3(int num1, int num2, int num3){
+
+    /**
+     * Devuelve el número mayor de los tres números
+     * @param num1 el primer número
+     * @param num2 el segundo número
+     * @param num3 el tercer número
+     * @return el número más grande de los 3
+     */
+    public static int mayorMenor3(int num1, int num2, int num3) {
         int maximo;
-        if (num1>num2 && num1>num3){
-        maximo = num1;
+
+        if (num1 > num2 && num1 > num3) {
+            maximo = num1;
+        } else if (num2 > num1 && num2 > num3) {
+            maximo = num2;
+        } else {
+            maximo = num3;
         }
-        if (num2>num1 && num2>num3){
-        maximo = num2;
-        }
-        else{
-        maximo = num3;
-        }
-        return(maximo);
+
+        return maximo;
     }
+
+    /**
+     * Fuunción para números y mostrar el mayor de ellos utilizando lo establecido en la clase de arriba.
+     */
     public static void main(String[] args) {
-        {
+
         // Comparación de dos números
         System.out.println("Función de dos números.");
         Scanner sc = new Scanner(System.in);
@@ -36,18 +57,15 @@ public class ElementoMejoraEjercicioProg {
         System.out.println("Introduce otro número: ");
         int num2 = sc.nextInt();
         System.out.println("El número mayor es " + mayorMenor(num1, num2));
-        }
-        {
+
         // Comparación de tres números
         System.out.println("Función de tres números.");
-        Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un número: ");
-        int num1 = sc.nextInt();
+        int num3_1 = sc.nextInt();
         System.out.println("Introduce otro número: ");
-        int num2 = sc.nextInt();
+        int num3_2 = sc.nextInt();
         System.out.println("Introduce otro número: ");
-        int num3 = sc.nextInt();
-        System.out.println("El número mayor es " + mayorMenor3(num1, num2, num3));
-        }
+        int num3_3 = sc.nextInt();
+        System.out.println("El número mayor es " + mayorMenor3(num3_1, num3_2, num3_3));
     }
 }
